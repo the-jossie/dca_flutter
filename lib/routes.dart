d5b41773c78da1a307e43e2dbe93899d04e346a4
Dev Jo <idowuayanfeoluwa@gmail.com>
2022-10-08 12:59:07 +0100

    "wip: auth"

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,7 +1,7 @@
-import 'views/authentication.dart';
-import 'views/splash.dart';
+import 'pages/auth.dart';
+import 'pages/splash.dart';
 
 final routes = {
   "splash_screen": (context) => const SplashView(),
-  "sync_account": (context) => const AuthenticationView(),
+  "auth": (context) => const AuthView(),
 };
